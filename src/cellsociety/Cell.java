e674a1ef98c039b36d2d702718c1f74d21e1159c
Mike Liu <zl99@duke.edu>
2017-01-29 02:40:58 -0500

    Update Cell.java

--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -1,5 +1,27 @@
 package cellsociety;
 
+import javafx.scene.paint.Color;
+
+/**
+ * 
+ * @author Mike Liu
+ *
+ */
 public class Cell {
     
+    private int state;
+    private Color color;
+    
+    public Cell(int state, Color color) {
+        this.state = state;
+        this.color = color;
+    }
+    
+    public int getState() {
+        return state;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
 }
