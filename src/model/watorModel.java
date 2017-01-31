1acc66395021ad35219f22be662f15c240d64c29
Bilva Sanaba <bgs21@duke.edu>
2017-01-31 17:27:47 -0500

    Test merge from master

--- src/model/watorModel.java
+++ src/model/watorModel.java
@@ -3,7 +3,7 @@ package model;
 import cellsociety.Model;
 
 public class watorModel extends Model{
-
+	private final int SharkEnergy = 10;
 	@Override
 	public void step() {
 		// TODO Auto-generated method stub
