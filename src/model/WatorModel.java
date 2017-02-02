88860ad5ba7cd286b7c4d3204205f782ba974f52
Mike Liu <zl99@duke.edu>
2017-02-01 23:49:09 -0500

    Add WatorModel.java

---
+++ src/model/WatorModel.java
@@ -0,0 +1,15 @@
+package model;
+
+import cellsociety.Model;
+
+public class WatorModel extends Model {
+
+    private int sharkEnergy = 10;
+    
+    @Override
+    public void step() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
